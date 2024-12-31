15873aceda26652bad2a87d5fbd9b81703d8440e
Timo Heiß <ti-heiss@t-online.de>
2024-12-31 13:18:59 +0100

    Add custom ALE function and remove old pyALE based one

--- current_research_feature_effects/feature_effects.py
+++ current_research_feature_effects/feature_effects.py
@@ -1,12 +1,10 @@
-import warnings
 from typing_extensions import List, Dict, Callable
 import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator
-from PyALE import ale
 
 
-def _partial_dependence(estimator: BaseEstimator, X: np.ndarray, features: List[int], grid: List[np.ndarray]) -> Dict:
+def _partial_dependence(estimator: BaseEstimator, X: np.ndarray, feature: int, grid: np.ndarray) -> Dict:
     """
     Compute partial dependence of features in a dataset based on a provided grid of values.
 
@@ -16,9 +14,9 @@ def _partial_dependence(estimator: BaseEstimator, X: np.ndarray, features: List[
         Fitted model to compute partial dependence for.
     X : np.ndarray
         Dataset to compute partial dependence for.
-    features : List[int]
-        Indices of features to compute partial dependence for.
-    grid : List[np.ndarray]
+    feature : int
+        Index of feature to compute partial dependence for.
+    grid : np.ndarray
         Grid of values to compute partial dependence for.
 
     Returns
@@ -26,20 +24,82 @@ def _partial_dependence(estimator: BaseEstimator, X: np.ndarray, features: List[
     Dict
         Dictionary containing the grid values and the computed partial dependence values.
     """
-    if not isinstance(features, list):
-        features = [features]
+    pdp_values = np.zeros(len(grid))
 
-    grid_values = grid if isinstance(grid, list) else [grid]
-    pdp_values = np.zeros(len(grid_values[0]))
-
-    for i, value in enumerate(grid_values[0]):
+    for i, value in enumerate(grid):
         X_temp = X.copy()
-        X_temp[:, features[0]] = value
+        X_temp[:, feature] = value
 
         predictions = estimator.predict(X_temp)
         pdp_values[i] = np.mean(predictions)
 
-    return {"grid_values": grid_values[0], "effect": pdp_values}
+    return {"grid_values": grid, "effect": pdp_values}
+
+
+def _accumulated_local_effects(
+    estimator: BaseEstimator, X: pd.DataFrame, feature: str, grid: np.ndarray
+) -> pd.DataFrame:
+    """Compute the accumulated local effect of a numeric continuous feature.
+
+    This function is a customized version of the `aleplot_1D_continuous()` function
+    from the PyALE library. The original function can be found at:
+
+    https://github.com/DanaJomar/PyALE/blob/master/PyALE/_src/ALE_1D.py
+
+    (version 1.2.0)
+
+    Parameters
+    ----------
+    estimator : BaseEstimator
+        Fitted model to compute accumulated local effects for.
+    X : pd.DataFrame
+        Dataset to compute accumulated local effects for.
+    feature : str
+        Name of the feature to compute accumulated local effects for.
+    grid : np.ndarray
+        Grid of values to compute accumulated local effects for.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the grid values and the computed accumulated local effects values.
+
+    """
+    # modification: use custom grid
+    bins = np.unique(grid)
+    feat_cut = pd.cut(X[feature], bins, include_lowest=True)
+
+    bin_codes = feat_cut.cat.codes
+
+    X1 = X.copy()
+    X2 = X.copy()
+    X1[feature] = [bins[i] for i in bin_codes]
+    X2[feature] = [bins[i + 1] for i in bin_codes]
+    try:
+        y_1 = estimator.predict(X1).ravel()
+        y_2 = estimator.predict(X2).ravel()
+    except Exception:
+        raise Exception("Please check that your model is fitted, and accepts X as input.")
+
+    delta_df = pd.DataFrame({feature: bins[bin_codes + 1], "Delta": y_2 - y_1})
+
+    # modification to also include empty bins:
+    all_grid_points = pd.DataFrame({feature: bins})  # include all points
+    # Compute means and sizes for each grid point, ensuring all points are preserved
+    grouped = delta_df.groupby([feature], observed=False)
+    means = grouped.Delta.mean().reindex(all_grid_points[feature]).fillna(0)
+    sizes = grouped.Delta.size().reindex(all_grid_points[feature]).fillna(0)
+    res_df = pd.DataFrame({"eff": means, "size": sizes})
+
+    res_df["eff"] = res_df["eff"].cumsum()
+    res_df.loc[min(bins), :] = 0
+    # subtract the total average of a moving average of size 2
+    mean_mv_avg = ((res_df["eff"] + res_df["eff"].shift(1, fill_value=0)) / 2 * res_df["size"]).sum() / res_df[
+        "size"
+    ].sum()
+    res_df = res_df.sort_index().assign(eff=res_df["eff"] - mean_mv_avg)
+
+    return res_df
 
 
 def compute_pdps(
@@ -48,6 +108,7 @@ def compute_pdps(
     feature_names: List[str],
     grid_values: List[np.ndarray],
     center_curves: bool = False,
+    remove_first_last: bool = False,
 ) -> List[Dict]:
     """
     Compute partial dependence plots for a given model and dataset.
@@ -64,6 +125,9 @@ def compute_pdps(
         Grid of values to compute partial dependence for.
     center_curves : bool, optional
         Whether to center the PDP curves by substracting their mean or not, by default False.
+    remove_first_last : bool, optional
+        Whether to remove the first and last grid values from the PDP curves before centering and
+        returning or not, by default False.
 
     Returns
     -------
@@ -75,9 +139,12 @@ def compute_pdps(
         pdp_feature = _partial_dependence(
             estimator=model,
             X=X,
-            features=[feature],
-            grid=[grid],
+            feature=feature,
+            grid=grid,
         )
+
+        if remove_first_last:
+            pdp_feature["effect"] = pdp_feature["effect"][1:-1]
 
         if center_curves:
             pdp_feature["effect"] -= np.mean(pdp_feature["effect"])
@@ -97,8 +164,9 @@ def compute_ales(
     model: BaseEstimator,
     X: np.ndarray,
     feature_names: List[str],
-    grid_intervals: int,
+    grid_values: List[np.ndarray],
     center_curves: bool = False,
+    remove_first_last: bool = False,
 ) -> List[Dict]:
     """
     Compute accumulated local effects for a given model and dataset.
@@ -111,10 +179,13 @@ def compute_ales(
         Dataset to compute accumulated local effects for.
     feature_names : List[str]
         Names of features to compute accumulated local effects for.
-    grid_intervals : int
-        Number of intervals to compute accumulated local effects for.
+    grid_values : List[np.ndarray]
+        Grid of values to compute accumulated local effects for.
     center_curves : bool, optional
         Whether to center the ALE curves by substracting their mean or not, by default False.
+    remove_first_last : bool, optional
+        Whether to remove the first and last grid values from the ALE curves before centering and
+        returning or not, by default False.
 
     Returns
     -------
@@ -123,17 +194,16 @@ def compute_ales(
     """
     ales = []
     X_df = pd.DataFrame(X, columns=feature_names)
-    for feature in X_df.columns:
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", category=UserWarning)
-            ale_feature = ale(
-                X=X_df,
-                model=model,
-                feature=[feature],
-                grid_size=grid_intervals,
-                plot=False,
-                include_CI=False,
-            )
+    for feature, grid in zip(feature_names, grid_values):
+        ale_feature = _accumulated_local_effects(
+            estimator=model,
+            X=X_df,
+            feature=feature,
+            grid=grid,
+        )
+
+        if remove_first_last:
+            ale_feature = ale_feature.iloc[1:-1]
 
         if center_curves:
             ale_feature["eff"] -= np.mean(ale_feature["eff"])
